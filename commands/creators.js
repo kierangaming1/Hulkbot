c9c6d46a5951cb7b52f4995dceac1d23cc2b6d34
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-22 14:55:41 +0100

    Update creators.js

--- commands/creators.js
+++ commands/creators.js
@@ -1,5 +1,5 @@
 module.exports.run = (bot, message, args, discord) => {
-  message.channel.send(`My creators are <@${process.env.oid}> and <@140487710727995392>.`)
+  message.channel.send(`My creators are <@${process.env.oid}> and <@203283964771696640>.`)
 }
 
 module.exports.help = {
