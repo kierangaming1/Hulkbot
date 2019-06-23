887acfab75b0196ed4650f3853ed899ee645f516
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 12:18:57 +0100

    Update snek.js

--- commands/snek.js
+++ commands/snek.js
@@ -1,6 +1,6 @@
  module.exports.run = (bot, message, args, discord) => {
   let em = new discord.RichEmbed()
-  .setTitle(`Hulkbot Snek`)
+  .setTitle(`PieCord Snek`)
   .setDescription(`I am big snek. Are you?`)
   .setImage("https://media.giphy.com/media/26DMWpQjDSf4AI9Ik/giphy.gif")
   .setThumbnail(bot.user.avatarURL)
