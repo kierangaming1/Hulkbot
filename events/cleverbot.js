9b9d1d0ca3521049c21ea551fed63a1f5d924861
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 13:29:37 +0100

    Update cleverbot.js

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -1,18 +1,18 @@
 module.exports = (bot, message, cb, prefix) => {
   if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
-  cb.setNick("Hulkbot")
+  cb.setNick("PieCord")
   if (!message.content.toLowerCase().includes("who is your creator")) {
         cb.create((err, session) => {
         cb.ask(message.content, (err, response) => {
         message.channel.send(response)
-        console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and Hulkbot responded with ${response}!`)
+        console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and PieCord responded with ${response}!`)
         if (err) {
           console.error(err)
         }
       })
     })
   } else {
-    message.channel.send(`My creators are <@${process.env.oid}> and <@140487710727995392>.`)
+    message.channel.send(`My creators are <@${process.env.oid}> and <@324924213410660355>.")
   }
 }
