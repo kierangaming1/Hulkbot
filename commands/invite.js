8a5d9985880854418a8bc4816578edbbae924c44
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 12:40:55 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,8 +2,8 @@ const discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
   let embed = new discord.RichEmbed()
-  .setTitle("Hulkbot Invitation")
-  .setDescription(`I can't use invite links.\nHere's an OAuth2 link instead! Click [here](https://discordapp.com/api/oauth2/authorize?client_id=294194506113220608&permissions=8&redirect_uri=https%3A%2F%2Fbot.hulkbot.ml%2Fhome&response_type=code&scope=bot%20guilds) to invite me!`)
+  .setTitle("PieCord Invitation")
+  .setDescription(`I can't use invite links.\nHere's an OAuth2 link instead! Click [here](https://discordapp.com/oauth2/authorize?client_id=548622504659255297&scope=bot&permissions=842279998) to invite me!`)
   .setColor("GREEN")
   .setFooter(`Invite Command`)
   .setTimestamp()
