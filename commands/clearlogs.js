5270da120131fb93a709dfb5aeee0c2cd6dd41ea
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 13:11:33 +0100

    Update clearlogs.js

--- commands/clearlogs.js
+++ commands/clearlogs.js
@@ -3,7 +3,7 @@ const prefix = process.env.prefix
 
 module.exports.run = (bot, message, args, discord) => {
   if (!config.owners.includes(message.author.id)) return message.channel.send("Nope!")
-  if (message.channel.id == "356181785899565077") {
+  if (message.channel.id == "592308975949840393") {
       message.channel.bulkDelete(100).catch(console.error)
       message.channel.send("Successfully cleared the logs.").then(m => m.delete(5000))
     } else {
@@ -13,6 +13,6 @@ module.exports.run = (bot, message, args, discord) => {
 
 module.exports.help = {
   name: "clearlogs",
-  usage: "h!clearlogs",
+  usage: "!clearlogs",
   information: "Clears the bot logs"
 }
