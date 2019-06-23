b0c792f14048f529e3be1c1356d8c9cc418b9d99
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 13:31:19 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -14,10 +14,10 @@ module.exports = (bot, guild, discord) => {
   // Owner msg
   let member = guild.owner
   let embed = new discord.RichEmbed()
-  .setTitle("Introduction to Hulkbot")
+  .setTitle("Introduction to Piecord")
   .addField("Heyo! Thanks for adding me.","Before you start using me, I need to get some things straight. First of all, for some basic info about me, say `h!info`.")
-  .addField("To see a list of commands","Say `h!help`.", true)
-  .addField("If you need to report a bug:","go to the [issue tracker](https://github.com/FHGDev/JSHulkbot/issues/new), or just use the `h!contact` command, and it will send a message straight to my creator.")
+  .addField("To see a list of commands","Say `!help`.", true)
+  .addField("If you need to report a bug:", "Use the `!contact` command, and it will send a message straight to my creator.")
   .addField("Done!", "Alrighty! Now you can start using all my awesome features in your server!")
   .setThumbnail(bot.user.avatarURL)
   .setTimestamp()
@@ -25,7 +25,7 @@ module.exports = (bot, guild, discord) => {
   
   const newD = new gdata({
     guildId: guild.id,
-    prefix: "h!",
+    prefix: "!",
     isPremium: false,
     welcome: channel.id || null,
     modlog: modc.id || null,
