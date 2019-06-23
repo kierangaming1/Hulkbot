8204f1fca057397ee9ccefbb36342328c2c8e57c
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 12:27:36 +0100

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -1,6 +1,6 @@
 module.exports.run = (bot, message, args) => {
   const result = Math.round(Math.random())
-  if (message.content.startsWith("h!question")) {
+  if (message.content.startsWith("!question")) {
     if (result) {
     message.channel.send("Yes!")
   } else {
