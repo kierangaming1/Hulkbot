0ee8b81538f229f2f4e7afb79f9ae10f286c84d8
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 15:15:53 +0100

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -2,7 +2,7 @@ const code = require("../json/redeem/codes.json").codes
 
 module.exports.run = (bot, message, args, discord) => {
   let msg = args.join(" ")
-  if (codes.includes(msg)) {
+  if (code.includes(msg)) {
     let em = new discord.RichEmbed()
     .setTitle(`${bot.user.displayName} Code Redemption`)
     .setDescription(`Correct Code! Use this invite to go to the PieCord Base discord where you'll get the Yay role, and more!`)
