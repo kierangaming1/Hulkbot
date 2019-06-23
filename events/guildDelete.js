8f49fb75ef48fc82430dd93b6cb860eac7df3072
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-23 13:32:06 +0100

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 module.exports = (bot, guild, discord) => {
   let embed = new discord.RichEmbed()
-  .setTitle("Hulkbot's Final Message.")
-  .setDescription("Aw, sorry you weren't satisfied with me... Please tell my creator what's wrong by saying `h!contact msg`, or just going to https://github.com/FHGDev/JSHulkbot/issues/new. Thanks!")
+  .setTitle("PieCord's Final Message.")
+  .setDescription("Aw, sorry you weren't satisfied with me... Please tell my creator what's wrong by saying `!contact msg`, . Thanks!")
   .setAuthor(bot.user.username)
   .setTimestamp()
   .setColor("RED")
