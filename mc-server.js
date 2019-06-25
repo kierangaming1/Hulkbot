cd60e2ce526bac29c6c4ffad3e7217d3653f7698
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2019-06-25 18:33:41 +0100

    Create mc-server.js

---
+++ mc-server.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var request = require('request');
+var mcCommand = '!minecraft'; // Command for triggering
+var mcIP = '176.9.18.234'; // Your MC server IP or hostname address
+var mcPort = 25569; // Your MC server port (25565 is the default)
+
+client.on('message', message => {
+    if (message.content === mcCommand) {
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('Error getting Minecraft server status...');
+            }
+            body = JSON.parse(body);
+            var status = '*Minecraft server is currently offline*';
+            if(body.online) {
+                status = '**Minecraft** server is **online**  -  ';
+                if(body.players.now) {
+                    status += '**' + body.players.now + '** people are playing!';
+                } else {
+                    status += '*Nobody is playing!*';
+                }
+            }
+            message.reply(status);
+        });
+    }
+});
